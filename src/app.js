5c85bf13c1c5bf08bbfdb87399fe15779db90573
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-07 23:59:40 -0300

    Feat: db connection

--- src/app.js
+++ src/app.js
@@ -21,6 +21,16 @@ try{
 const db = mongoClient.db();
 
 
+// - [ ]  O usuário deve ser capaz de criar uma conta na tela de rota `/cadastro`.
+// - [ ]  Através de uma requisição **POST**, realize o cadastro do usuário no banco de dados.
+//     - [ ]  Todos os campos são obrigatórios. Faça validações de acordo com a necessidade no front e no back que garantam que todos os dados estejam presentes.
+//     - [ ]  O e-mail deve ter um formato válido. Caso não possua, a requisição deve retornar o status code `422 (Unprocessable Entity)`e o front-end deve mostrar uma mensagem explicando o erro. (Use `alert`)
+//     - [ ]  A senha deve possuir no mínimo três caracteres. Caso não possua, a requisição deve retornar status `422 (Unprocessable Entity)`e o front-end deve mostrar uma mensagem explicando o erro. (Use `alert`)
+//     - [ ]  Caso já exista um usuário com este e-mail cadastrado, a requisição deve retornar status code `409 (Conflict)` e o front-end deve mostrar uma mensagem explicando o erro. (Use `alert`)
+//     - [ ]  Caso algum dos campos não esteja presente ou esteja em um formato inválido, a requisição deve retornar status code `422 (Unprocessable Entity)` e o front-end deve mostrar uma mensagem explicando. (Use `alert`)
+//     - [ ]  Em caso de sucesso, a requisição deve retornar status `201 (Created)`.
+// - [ ]  Caso as senhas inseridas nos campos de *senha* e *confirme senha* não sejam iguais, a requisição não deve ser feita. Valide no front-end se esses valores são iguais e, somente então, envie sua requisição para a API (não é necessário enviar a senha duas vezes).
+// - [ ]  Realize a criptografia da senha do usuário para guardá-la no banco de dados.
 
 
 app.listen(5000, () => {console.log("Server is running on port 5000")});

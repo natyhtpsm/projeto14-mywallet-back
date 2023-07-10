a0ed98a9ce0513322f3abe4d0623737ea223f6a2
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-10 18:18:46 -0300

    Update: addtransaction controller, middleware, repository, router

--- src/controllers/addTransactionController.js
+++ src/controllers/addTransactionController.js
@@ -3,12 +3,11 @@ import { addTransaction } from "../repository/transactionRepository.js";
 export async function addTransationController(req, res){
     const { valor, descricao } = req.body;
     const { tipo } = req.params; 
+    const {userId} = res.locals.session;
 
     const transaction = addTransaction( valor, descricao, tipo, userId ); 
     if(!transaction){
         return res.sendStatus(500);
     }
-
-    return res.sendStatus(200);
-    //REDIRECIONAR PARA A PAGINA HOME
+    return res.sendStatus(201);
 }
--- src/middlewares/addTransactionMiddleware.js
+++ src/middlewares/addTransactionMiddleware.js
@@ -6,11 +6,10 @@ export async function addTransactionMiddleware(req, res, next){
     const { tipo } = req.params; 
     const sessionData = res.locals.session;
     const token = sessionData.token;
-    const userId = sessionData.userId;
     const tokenexists = tokenExists(token);
     const schemaValidation = transactionSchema.validate({valor, descricao}); 
     if(!schemaValidation){ return res.sendStatus(422); }
-    if(!tokenexists){ return res.sendStatus(401); }
+    // if(!tokenexists){ return res.sendStatus(401); }
     if(tipo !== "entrada" || tipo !=="saida"){ return res.sendStatus(422); } 
     next();
 };
